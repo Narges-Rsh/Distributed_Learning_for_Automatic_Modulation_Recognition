f041b8c0eb81cec4dcae20921fdd93239fa6c567
Narges-Rsh <134446921+Narges-Rsh@users.noreply.github.com>
2024-04-12 10:48:28 -0400

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+from .ModelBase import ModelBase
+# CNN
+
+from .CNN1 import CNN1
+from .Voting import Voting
+from .Feature_8 import Feature_8
+
+__all__ = ['ModelBase','CNN1','Feature_8','Voting']
