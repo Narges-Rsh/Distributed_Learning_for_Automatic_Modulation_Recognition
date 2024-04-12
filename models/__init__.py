862011c6e450e39608abaa92d0e453c94d9be97c
Narges-Rsh <134446921+Narges-Rsh@users.noreply.github.com>
2024-04-12 14:24:48 -0400

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+from .ModelBase import ModelBase
+# CNN
+
+from .VTCNN2_1D import VTCNN2_1D
+from .DAMR_V import DAMR_V
+from .DAMR_F8 import DAMR_F8
+
+__all__ = ['ModelBase','VTCNN2_1D','DAMR_F8','DAMR_V']
